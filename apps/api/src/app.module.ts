4e3c312e4744aae714fd0db8b941d3adb3d3a250
sin_ <93648188+yaxxsin@users.noreply.github.com>
2026-04-29 10:26:00 +0700

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -69,6 +69,7 @@ import { BullModule } from '@nestjs/bullmq';
         connection: {
           host: config.get<string>('REDIS_HOST', 'localhost'),
           port: config.get<number>('REDIS_PORT', 6379),
+          password: config.get<string>('REDIS_PASSWORD', ''),
         },
       }),
       inject: [ConfigService],
